b143d1ff3988f7089c0895166bd88a17e2a346f0
Alexander Esteban <142831253+este6795@users.noreply.github.com>
2025-10-22 19:50:57 +0000

    Connect front to back

---
+++ Frontend/frontend/src/components/ProductList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/api/products")
+        .then((response) => response.json()) .then((data) => setProducts(data))
+        .catch((error) => console.error("Error fetching products:", error));
+    }, []);
+  
+    return (
+    <div>
+        <h1>Product List</h1>
+    <ul>
+    {products.map((product) => (
+        <li key={product.id}> {product.name} - ${product.price} (Qty: {product.qty}) </li>
+    ))}
+    </ul>
+    </div>
+    )
+}
+export default ProductList
